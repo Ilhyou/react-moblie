95d326a6314976f1c9d90011d88966775cea354f
齐林河 <1277859500@qq.com>
2019-07-07 16:22:29 +0800

    首页动态渲染完成

--- src/api/index.js
+++ src/api/index.js
@@ -5,3 +5,8 @@ import axios from "axios";
  * 获取首页轮播图的数据
  */
 export const getGoods = () => axios.get("http://react.zbztb.cn/site/goods/gettopdata/goods");
+
+/**
+ * 获取商品列表
+ */
+export const getGoodsGroup = () => axios.get("http://react.zbztb.cn/site/goods/getgoodsgroup");
